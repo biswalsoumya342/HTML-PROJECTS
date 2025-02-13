54e59e71e4ceb779cd9f68dfe06321a29c1f690b
Soamyaranjan Biswal <125767365+biswalsoumya342@users.noreply.github.com>
2025-02-13 09:36:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function firstQuote(){
+    quoteDisplay.textContent = "Quick solutions coupled with extraordinary performance-а recommendation that's unequivocal.";
+}
+function secondQuote(){
+    quoteDisplay.textContent = "We are absolutely thrilled with the services provided by Babun! Their team went above and beyond to transform.";
+}
+function thirdQuote(){
+    quoteDisplay.textContent = "Babun Solutions has truly been a game changer for our business. Their expertise in developing our mobile app.";
+}
